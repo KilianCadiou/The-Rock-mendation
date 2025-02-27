ef529745101bc6ac9dee3ff8c87892c35a56734a
KilianCadiou <kiliancadiou@gmail.com>
2025-02-27 12:03:08 +0100

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_pages import get_nav_from_toml
 
-nav = get_nav_from_toml(".streamlit/pages.toml")
+nav = get_nav_from_toml("streamlit/.streamlit/pages.toml")
 
 st.logo("streamlit/img/logo.png")
 
